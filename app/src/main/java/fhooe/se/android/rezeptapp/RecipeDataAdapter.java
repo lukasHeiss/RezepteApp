21648eee90fa319908d2fd3e0ba88b4e6ac423c8
Lukas <lukas.heiss@students.fh-hagenberg.at>
2018-01-11 14:09:11 +0100

    Create RecipeDataAdapter

--- app/src/main/java/fhooe/se/android/rezeptapp/RecipeDataAdapter.java
+++ app/src/main/java/fhooe/se/android/rezeptapp/RecipeDataAdapter.java
@@ -1,8 +1,35 @@
 package fhooe.se.android.rezeptapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 /**
  * Created by Lukas on 11.01.18.
  */
 
-public class RecipeDataAdapter {
+public class RecipeDataAdapter extends ArrayAdapter<RecipeData> {
+    private View.OnClickListener mListener;
+
+
+    public RecipeDataAdapter(Context _c, View.OnClickListener _listener) {
+        super(_c, -1);
+        mListener = _listener;
+    }
+
+
+    @Override
+    public View getView(int _pos, View _view, ViewGroup _parent) {
+        if (_view == null){
+            Context c = getContext();
+            LayoutInflater inflater =
+        }
+        //return super.getView(position, convertView, parent);
+    }
 }
