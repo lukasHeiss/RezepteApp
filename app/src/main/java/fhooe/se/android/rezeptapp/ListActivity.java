ccfd8ec91b4897cc7fd1e75fff1833d8ccfd02e4
Lukas <lukas.heiss@students.fh-hagenberg.at>
2018-01-11 14:34:47 +0100

    Create RecipeDataAdapter tests

--- app/src/main/java/fhooe/se/android/rezeptapp/ListActivity.java
+++ app/src/main/java/fhooe/se/android/rezeptapp/ListActivity.java
@@ -2,12 +2,41 @@ package fhooe.se.android.rezeptapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ListActivity extends Activity {
+public class ListActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
+    public static final String TAG="Rezepte-Liste";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ArrayAdapter adapter = new RecipeDataAdapter(this, this);
+
+        adapter.add(new RecipeData(1,"Pizza", -1));
+        adapter.add(new RecipeData(1,"Tom", -1));
+
+        ListView lv = (ListView)findViewById(R.id.activity_list_mainview);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> _adapterView,
+                            View _view,
+                            int _i,
+                            long _l) {
+        RecipeData data = (RecipeData) _adapterView.getAdapter().getItem(_i);
+        Toast.makeText(this, data.getRecipeName() + " has been selected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View _view) {
+        Toast.makeText(this,"Properties has been selected", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/fhooe/se/android/rezeptapp/RecipeDataAdapter.java
+++ app/src/main/java/fhooe/se/android/rezeptapp/RecipeDataAdapter.java
@@ -28,8 +28,21 @@ public class RecipeDataAdapter extends ArrayAdapter<RecipeData> {
     public View getView(int _pos, View _view, ViewGroup _parent) {
         if (_view == null){
             Context c = getContext();
-            LayoutInflater inflater =
+            LayoutInflater inflater = (LayoutInflater)
+                    getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            _view = inflater.inflate(R.layout.activity_list_element, null);
         }
+        final RecipeData data = getItem(_pos);
+        if (data != null){
+            TextView tv = null;
+            tv = (TextView)_view.findViewById(R.id.activity_list_element_textview_name);
+            tv.setText(data.getRecipeName());
+            ImageView iv = null;
+            iv=(ImageView) _view.findViewById(R.id.activity_list_element_icon);
+            iv.setImageResource(data.getIcon());
+        }
+
+        return _view;
         //return super.getView(position, convertView, parent);
     }
 }
