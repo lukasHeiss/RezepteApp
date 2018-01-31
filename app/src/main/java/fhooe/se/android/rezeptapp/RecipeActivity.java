4fcb322b7543a8a62b142beff2900654a91e6df8
Tom Hacker <tom.hacker.tom@gmail.com>
2018-01-31 17:52:08 +0100

    #7 Implemented working Portion calculator

--- app/src/main/java/fhooe/se/android/rezeptapp/RecipeActivity.java
+++ app/src/main/java/fhooe/se/android/rezeptapp/RecipeActivity.java
@@ -1,33 +1,28 @@
 package fhooe.se.android.rezeptapp;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
-import android.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 
+import java.util.Locale;
+
 import fhooe.se.android.rezeptapp.DAL.DALFactory;
-import fhooe.se.android.rezeptapp.DAL.DataManager;
 import fhooe.se.android.rezeptapp.DAL.IDataManager;
+import fhooe.se.android.rezeptapp.DAL.IngredientData;
 import fhooe.se.android.rezeptapp.DAL.IngredientDataAdapter;
 import fhooe.se.android.rezeptapp.DAL.InstructionDataAdapter;
-import fhooe.se.android.rezeptapp.DAL.RecipeData;
 import fhooe.se.android.rezeptapp.DAL.RecipeExtendedData;
 
 public class RecipeActivity extends Activity implements RecipeDataCallBack{
@@ -85,6 +80,29 @@ public class RecipeActivity extends Activity implements RecipeDataCallBack{
         return view;
     }
 
+    public void onClickMinus(View v)
+    {
+        data.setBasePortions(data.getBasePortions() - 1);
+        updateData();
+    }
+
+    public void onClickPlus(View v)
+    {
+        data.setBasePortions(data.getBasePortions() + 1);
+        updateData();
+    }
+
+    public void updateData() {
+        ((TextView) this.findViewById(R.id.activity_recipe_portion_text)).setText("" + data.getBasePortions() + " " + getResources().getString(R.string.servings));
+
+        LinearLayout linear = (LinearLayout) this.findViewById(R.id.activity_recipe_ingredientList);
+        for(int i = 0; i < linear.getChildCount(); i++){
+            TextView tv = (TextView) linear.getChildAt(i).findViewById(R.id.activity_recipe_ingredientelement_number);
+            IngredientData ingr = data.getIngredientList().get(i);
+            tv.setText(String.format(Locale.getDefault(),"%.0f%s", ingr.getAmount() * data.getBasePortions(), ingr.getUnit()));
+        }
+    }
+
     @Override
     public void dataLoaded(RecipeExtendedData recipe) {
         data = recipe;
@@ -104,7 +122,19 @@ public class RecipeActivity extends Activity implements RecipeDataCallBack{
                 metaElements.addView(getMetaElementView(String.valueOf(data.getTimePreparation()) + " m " + getResources().getString(R.string.prepTime), R.drawable.ic_clock));
             if(data.getDifficulty() > 0)
                 metaElements.addView(getMetaElementView(getResources().getString(data.getDifficulty()), R.drawable.ic_chef));
-            metaElements.addView(getMetaElementView(data.getBasePortions() + getResources().getString(R.string.servings), R.drawable.ic_cutlery));
+
+            LayoutInflater inflater = (LayoutInflater)
+                    this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.activity_recipe_portions, null);
+            iv = (ImageView) view.findViewById(R.id.activity_recipe_portion_icon);
+            iv.setImageResource(R.drawable.ic_cutlery);
+
+            tv = (TextView) view.findViewById(R.id.activity_recipe_portion_text);
+            tv.setText(data.getBasePortions() + " " + getResources().getString(R.string.servings));
+
+            metaElements.addView(view);
+
+
 
 
             //add the List of instructions
@@ -122,6 +152,7 @@ public class RecipeActivity extends Activity implements RecipeDataCallBack{
             for(int i = 0; i < ingredientsAdapter.getCount(); i++){
                 ingredients.addView(ingredientsAdapter.getView(i, null, ingredients));
             }
+            updateData();
         }
     }
 }
